5bdbfef266e0a52a99bb2c3f61228f873444882f
dglink25 <dglink25@gmail.com>
2025-12-11 09:36:30 +0100

    Problème npm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      inject: {
+        injectData: {
+          appUrl: process.env.APP_URL,
+        },
+      },
+    }),
+  ],
+  build: {
+    outDir: 'public/build',  // Spécifie le répertoire de sortie
+    manifest: true,  // Cela génère le fichier manifest.json
+  },
+  server: {
+    proxy: {
+      '/app': 'http://localhost',
+    },
+  },
 });
